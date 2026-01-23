a26a87d84c15d5e4f7b023fe6f192dfd2916b66f
SoumyadeepChakraavarti <Soumyadeepsai1@gmail.com>
2026-01-24 01:42:41 +0530

    update

---
+++ app/src/main/java/com/nexus/app/Main.java
@@ -0,0 +1,10 @@
+package com.nexus.app;
+
+import com.nexus.core.Core;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Core.hello());
+    }
+}
+
---
+++ core/src/main/java/com/nexus/core/Core.java
@@ -0,0 +1,8 @@
+package com.nexus.core;
+
+public class Core {
+    public static String hello() {
+        return "Hello from core";
+    }
+}
+
